403b4fc6f5f6c84d8e599db72e0c71f5ecb5d0eb
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-10-02 14:24:06 +0200

    Add project files.

---
+++ Wekker/Program.cs
@@ -0,0 +1,82 @@
+﻿using Wekker;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        WekkerModus callback;
+
+        Timer timer = new Timer(wm1);
+        
+        
+
+        char keuze = '0';
+        do
+        {
+
+            
+            Console.WriteLine("WEKKERINSTELLINGEN");
+            Console.WriteLine("------------------");
+            Console.WriteLine("0 Instellen compleet");
+            Console.WriteLine("1 Alarmtijd instellen");
+            Console.WriteLine("2 stop alarm - sluimerknop");
+            Console.WriteLine("3 wekkeralarm-tekstmodus aan/uit");
+            Console.WriteLine("4 wekkeralarm-knippermodus aan/uit");
+            Console.WriteLine("5 wekkeralarm-geluidsmodus aan/uit");
+            Console.Write("Geef je keuze in en druk [Enter]: ");
+            keuze = Console.ReadLine()[0];
+            Console.WriteLine();
+
+            switch(keuze)
+            {
+
+                case '0':
+                    break;
+
+                case '1':
+                    Console.WriteLine("Geef de tijd in waarop je een alarm wil instellen.");
+                    int timeInt = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Geef de tijd in waarop je de sluimerstand wil activeren.");
+                    int timeInt2 = Convert.ToInt32(Console.ReadLine());
+                    //timer.Interval = TimeSpan.FromSeconds(3).TotalSeconds;
+                    timer.Change(timeInt2, timeInt);
+                    break;
+
+                case '2':
+                   // timer.Stop();
+                   timer.Dispose();
+                    Console.WriteLine("Timer afgezet.");
+                    break;
+
+                case '3':
+
+                    callback = new WekkerModus(Wekkertje.wekkertekst);
+                    break;
+
+                case '4':
+                    callback = new WekkerModus(Wekkertje.wekkerblink);
+                   
+                    break;
+
+                case '5':
+                   // callback = new WekkerModus(Wekkertje.wekkerGeluid);
+                    break;
+
+                default: 
+                    break;
+            }
+        } while (keuze != '0');
+
+        
+    }
+
+    private static void wm1(object? state)
+    {
+
+        WekkerModus callback;
+    }
+
+    delegate string WekkerModus(string text);
+}
+
---
+++ Wekker/Wekker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wekker
+{
+    internal class Wekkertje
+    {
+        delegate int AlarmTijden(int input);
+
+        delegate string WekkerModus(char optie);
+
+
+        
+
+        public static void wekkerGeluid()
+        {
+            Console.Beep(5000, 2000);
+            
+        }
+
+        public static string wekkerblink(string text)
+        {
+            text = "blink";
+            return text;
+        }
+
+        public static string wekkertekst(string text)
+        {
+            text = "ALARM! UIT BED!";
+            return text;
+        }
+
+    }
+}
